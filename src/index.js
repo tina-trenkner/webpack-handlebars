691138cb6729b1909fdf3808e0503eba48f97a9d
Rich Churcher <rich.churcher@gmail.com>
2016-08-09 17:47:43 +1200

    Stuff from this afternoon's session

--- src/index.js
+++ src/index.js
@@ -1,14 +1,58 @@
 var wombleList = require('./templates/womble-list.hbs')
+var detailsTemplate = require('./templates/details.hbs')
 
 document.addEventListener('DOMContentLoaded', function () {
-  var div = document.createElement('div')
-  var data = {
-    wombles: [
-      { name: 'Orinocc', email: 'orinocco@wimbledoncommon.net' },
-      { name: 'Toms', email: 'tomsk@wimbledoncommon.net' },
-      { name: 'Bung', email: 'bungo@wimbledoncommon.net' }
-    ]
-  }
-  div.innerHTML = wombleList(data)
-  document.body.appendChild(div)
+  var wombles = [
+    { name: 'Orinocco', email: 'orinocco@wimbledoncommon.net', details: 'Tin cans' },
+    { name: 'Tomsk', email: 'tomsk@wimbledoncommon.net', details: 'Plastic bags' },
+    { name: 'Bungo', email: 'bungo@wimbledoncommon.net', details: 'Discarded wombat poop' }
+  ]
+  refreshContent(wombles)
 })
+
+function refreshContent (wombles) {
+  var div = document.createElement('div')
+  div.innerHTML = wombleList({ wombles: wombles })
+  document.body.appendChild(div)
+  bindEventListeners(wombles)
+}
+
+function bindEventListeners (wombles) {
+  var lis = document.getElementsByTagName('li')
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].addEventListener('click', function (e) {
+      wombleToggle(e.target, wombles)
+    })
+  }
+}
+
+function wombleToggle (target, wombles) {
+  console.log(target.lastChild.tagName)
+  if (target.lastChild.tagName === 'UL') {
+    target.removeChild(target.lastChild)
+    return
+  }
+  appendDetails(target, wombles)
+}
+
+function appendDetails (target, wombles) {
+  var match = null
+  for (var i = 0; i < wombles.length; i++) {
+    if (wombleMatch(target.innerHTML, wombles[i])) {
+      match = wombles[i]
+    }
+  }
+  expandWomble(match, target)
+}
+
+function wombleMatch (innerHTML, wombat) {
+  return innerHTML.includes(wombat.email)
+}
+
+function expandWomble (match, target) {
+  if (match) {
+    var ul = document.createElement('ul')
+    ul.innerHTML = detailsTemplate(match)
+    target.appendChild(ul)
+  }
+}
